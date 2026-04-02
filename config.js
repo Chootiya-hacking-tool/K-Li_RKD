b2eb44583832c239ddcd008b90b9285cd3f3b026
Chootiya-hacking-tool <chamodmiyuru186@gmail.com>
2026-04-02 23:24:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,10 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+const config = {
+  SESSION_ID: "base64 හරි mega හරි session id එකක් මෙතනට දාන්න",
+  SESSION_NAME: "session id එකෙ මුලට දාලා තියෙන නම දාන්න Ex:- THARUSHA-MD=",
+  PREFIX: ".",
+  FOOTER: "> © Powerd by tharusha sandipa",
+  BOT_NAME: "Whatsapp Channel Song Upload Bot",
+  LOGO: "ඔයාගෙ image එකක direct url එකක් දාන්න මෙතනට"
 }
 
-module.exports = {
-    // 🔑 Essential Session & Owner Settings
-    SESSION_ID: process.env.SESSION_ID || "Enter your session id hear",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "94743404814",
-
-    // 🤖 Default Fallback Settings
-    DEFAULT_BOT_NAME: process.env.DEFAULT_BOT_NAME || "ZANTA-MD",
-    DEFAULT_OWNER_NAME: process.env.DEFAULT_OWNER_NAME || "Akash nimsara",
-    DEFAULT_PREFIX: process.env.DEFAULT_PREFIX || ".",
-
-    // 🖼️ Media Settings (Alive image & Menu)
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Akashkavindu/ZANTA_MD/blob/main/images/alive-new.jpg?raw=true",
-    MENU_IMG: process.env.MENU_IMG || "https://github.com/Akashkavindu/ZANTA_MD/blob/main/images/menu-new.jpg?raw=true",
-
-    // ⚙️ Dashboard / Database Defaults.
-    // මෙම අගයන් පළමු වතාවට Database එක සෑදීමේදී භාවිත වේ (Database & Dashboard Sync)
-    AUTO_READ: process.env.AUTO_READ || "false",
-    AUTO_TYPING: process.env.AUTO_TYPING || "false",
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-    READ_CMD: process.env.READ_CMD || "false",
-    AUTO_VOICE: process.env.AUTO_VOICE || "false",
-    ANTI_BADWORD: process.env.ANTI_BADWORD || "false",
-};
+module.exports = config;  
